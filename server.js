18133143a33edd8a075da3a12598e39bcf479196
Ian <mus5002@aol.com>
2019-12-18 21:45:11 -0500

    wrote sql queries, started api routes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const logger = require('./middleware/logger')
+const notFound = require('./middleware/notFound')
+const error = require('./middleware/error')
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
+server.use(logger())
 server.use(express.json());
+
+server.get('/', (req, res))
+
+server.use(notFound())
+server.use(error())
 
 module.exports = server;
